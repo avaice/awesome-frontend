3029870ed44412e01e0b59ae89ee570c1bc72757
avaice <dev@cho-ice.xyz>
2024-08-26 20:25:43 +0900

    install biomejs

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 export function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold">Vite + Preact</h1>
-      <p>hello, world</p>
-    </>
-  )
+	return (
+		<>
+			<h1 className="text-3xl font-bold">Vite + Preact</h1>
+			<p>hello, world</p>
+		</>
+	)
 }
